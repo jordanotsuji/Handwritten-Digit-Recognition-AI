4df862695b207692f216cfe6a11563a0b16f504c
jordanotsuji <18375877+jordanotsuji@users.noreply.github.com>
2022-11-01 17:55:16 -1000

    completed main method, fully functional

--- digit_guesser.py
+++ digit_guesser.py
@@ -6,6 +6,7 @@ WHITE = 0xFFFFFF
 BLACK = 0x000000
 WINDOW_WIDTH = WINDOW_HEIGHT = 600
 MODEL_PATH = "digit_recognition_128_128_10.model"
+model = tf.keras.models.load_model(MODEL_PATH)
 
 class Tile:
     def __init__(self, x, y, width, height):
@@ -63,6 +64,13 @@ class Canvas:
                 # initialize each tile with x and y values as tile width and height * row and column for automatic spacing
                 self.tiles[row].append(Tile(tile_width * column, tile_height * row, tile_width, tile_height))
 
+    def clear(self):
+        """
+        Clears the current canvas
+        """
+        self.tiles = []
+        self.initTiles()
+
     
     def convert_to_feature(self):
         """
@@ -87,3 +95,46 @@ class Canvas:
         return tf_compatable_feature
 
 
+def main():
+    """
+    Main loop detecting and responding to events 
+    """
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if(event.key == pygame.K_RETURN):
+                    # if enter is pressed, print the model's guess
+                    probabilities = model.predict(canvas.convert_to_feature())
+                    print(f'Predicted Probabilities: \n\t{probabilities[0]}')
+                    prediction = np.argmax(probabilities[0])
+                    print(f"Model Prediction: {prediction}")
+                elif(event.key == pygame.K_r):
+                    canvas.clear()
+
+            if pygame.mouse.get_pressed()[0]:
+                # if left click, color the tile black
+                pos = pygame.mouse.get_pos()
+                clicked = canvas.getTile(pos)
+                clicked.color = BLACK;
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit(0)
+
+        # redraw the canvas and update
+        canvas.draw(window)
+        pygame.display.update()
+
+canvas = Canvas(28, 28, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+# Pygame Window creation
+pygame.init()
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Handwritten Digit Classification AI")
+
+# main loop
+main()
+
+# Exit gracefully 
+pygame.quit()
+quit()
